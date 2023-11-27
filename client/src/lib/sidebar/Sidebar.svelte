5af4089b367fd4d1153c6836bb9bbfc44530e5d8
Bruno Gomes <brunomiguelgomes.bg8@gmail.com>
2023-11-27 15:53:49 +0000

    test branch

---
+++ client/src/lib/sidebar/Sidebar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { i, languages, switchLanguage } from '@inlang/sdk-js'
+
+    export let activePage: string
+
+    const pages = ["Reviews", "Statistics", "Models"]
+    const icons = [
+        "M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10",
+        "M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z",
+        "M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+    ]
+</script>
+
+<nav class="h-screen w-64 flex flex-col">
+    <div class="h-1/4 flex flex-col items-center justify-center">
+        <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+        </a>
+    </div>
+    <div class="h-2/4 flex flex-col items-center gap-y-1">
+        {#each pages as page, index}
+            <a href={`/${page.toLowerCase()}`} class="flex items-center gap-x-2 p-3 w-60 rounded-xl font-semibold text-black hover:text-white hover:bg-green-600 {activePage === page ? 'bg-green-600 text-white' : ''}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="{icons[index]}" />
+                </svg>
+                {i(page)}
+                {#if activePage === page}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-auto">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    </svg>
+                {/if}
+            </a>
+        {/each}
+    </div>
+    <div class="flex flex-col items-center">
+        {#each languages as lang}
+            <button on:click={() => switchLanguage(lang)}>{lang.toUpperCase()}</button>
+        {/each}
+    </div>
+</nav>
---
+++ client/src/lib/tables/ReviewsTable.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<div class="w-full h-full p-10">
+    <div class="bg-gray-100 rounded-lg p-4">
+        <input type="search" class="block w-full px-4 py-1 bg-white text-gray-500 text-sm text-center rounded-full" placeholder="Search" />
+    </div>
+</div>
